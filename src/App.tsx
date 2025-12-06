2a5fdaf949f0c3934e87ea6af58e8697c867e6c3
Harshil <harshilbhanushali11@gmail.com>
2025-12-06 13:42:55 +0530

    added animations b/w pages and removed side scroll bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,58 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
 import CustomCursor from './components/CustomCursor'
 import MenuButton from './components/MenuButton'
 import ThreeBackground from './components/ThreeBackground'
 import AboutPage from './pages/AboutPage'
 import MainPage from './pages/MainPage'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
+
+function AnimatedRoutes() {
+  const location = useLocation()
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <motion.div
+              initial={{ opacity: 0, scale: 0.98, filter: 'blur(10px)' }}
+              animate={{ opacity: 1, scale: 1, filter: 'blur(0px)' }}
+              exit={{ opacity: 0, scale: 1.02, filter: 'blur(10px)' }}
+              transition={{ duration: 0.4, ease: 'easeInOut' }}
+            >
+              <MainPage />
+            </motion.div>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <motion.div
+              initial={{ opacity: 0, scale: 0.98, filter: 'blur(10px)' }}
+              animate={{ opacity: 1, scale: 1, filter: 'blur(0px)' }}
+              exit={{ opacity: 0, scale: 1.02, filter: 'blur(10px)' }}
+              transition={{ duration: 0.4, ease: 'easeInOut' }}
+            >
+              <AboutPage />
+            </motion.div>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
+  )
+}
 
 function App() {
   return (
@@ -12,11 +61,9 @@ function App() {
         <ThreeBackground />
         <CustomCursor />
         <MenuButton />
+        <ScrollToTop />
         
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
+        <AnimatedRoutes />
       </div>
     </BrowserRouter>
   )
